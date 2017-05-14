4fb6eefa5f66b220d208dd924c25c6bb8180f8c5
bjpaul <kgec.bijoypaul@gmail.com>
2017-05-15 01:11:37 +0530

    Update Application.java

--- src/main/java/stateless/spring/security/Application.java
+++ src/main/java/stateless/spring/security/Application.java
@@ -11,7 +11,7 @@ public class Application extends SpringBootServletInitializer{
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class1);
+		return application.sources(Application.class);
 	}
 
 
